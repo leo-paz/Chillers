53284c7893fca32d5ca964af7a4bb8f006cca6d1
Mohamed Birama <mohamedbirama@Mohameds-MacBook-Pro.local>
2020-02-08 16:55:13 -0500

    init try to get list

--- mobile_app/src/screens/DashboardScreen.js
+++ mobile_app/src/screens/DashboardScreen.js
@@ -1,12 +1,49 @@
 import React, { useState, useEffect } from "react";
-import { Text, View, TouchableOpacity, SafeAreaView } from "react-native";
+import {
+  Text,
+  FlatList,
+  View,
+  TouchableOpacity,
+  SafeAreaView,
+  StyleSheet
+} from "react-native";
+
+const getFakepackages = () => {
+  return [
+    { name: "one" },
+    { name: "two" },
+    { name: "three" },
+    { name: "four" },
+    { name: "five" },
+    { name: "six" }
+  ];
+};
 
 const UserDashboardScreen = () => {
-  return(
+  packages = getFakepackages();
+  return (
     <SafeAreaView>
       <Text>DashboardScreen</Text>
+      <View style={styles.container}>
+        <FlatList
+          data={packages}
+          renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+        />
+      </View>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
 
 export default UserDashboardScreen;
